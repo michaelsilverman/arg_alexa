481242209bc9574501b836b3a8b2e28d4570c8a0
michaelsilverman <michael@sterlingresourcesgroup.net>
2017-01-02 17:04:35 -0500

    Updated messages

--- src/EventSubscriber/RequestSubscriber.php
+++ src/EventSubscriber/RequestSubscriber.php
@@ -29,21 +29,22 @@ class RequestSubscriber implements EventSubscriberInterface {
   public function onDebug(AlexaEvent $event) {
     \Drupal::logger('srg_alexa')->notice('this is the message');
     $request = $event->getRequest();
+    \Drupal::logger('srg_alexa')->notice('Intent: '.$request->intentName);
     $response = $event->getResponse();
     switch ($request->intentName) {
       case 'AMAZON.HelpIntent':
-        $response->respond('You can ask anything and I will respond with "Hello Drupal"');
+        $response->respond('xxx-You can ask anything and I will respond with "Hello Drupal"');
         break;
       case 'HelloDrupal':
         $city = $request->data['request']['intent']['slots']['City']['value'];
-        \Drupal::logger('srg_alexa')->notice('City: '.$city);
-        $response->respond('Welcome to '.$city);
+
+        $response->respond('Hello Drupal, welcome to '.$city);
         break;
       case 'GetDrupal':
         $city = $request->data['request']['intent']['slots']['City']['value'];
         $date = $request->data['request']['intent']['slots']['Date']['value'];
         $number = $request->data['request']['intent']['slots']['Number']['value'];
-        $response->respond('Welcome to '.$city.' Your number is:'.$number.' and the date is '.$date);
+        $response->respond('Get Drupal from '.$city.' Your number is:'.$number.' and the date is '.$date);
         break;
       default:
         $response->respond('The is the default'. $request->intentName.':');
